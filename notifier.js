466bbb7227f73482f35858296ccf858754a9146b
iminin <Igor.Minin@t-systems.ru>
2017-02-05 14:15:19 +0300

    Added more logic and moved constants to separate file

--- notifier.js
+++ notifier.js
@@ -1,57 +1,58 @@
-const express = require('express')
+'use strict';
+
+const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-// Constants
-const urlPath = 'notify';
-const port = 9999;
-const webHookUrl = 'https://hooks.slack.com/services/YOUR_URL';
+// Variables
+const variables = require('./variables');
+const slackUrl = 'https://slack.com/api/chat.postMessage';
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/' + urlPath, function (req, res) {
-    if (req.body) {
-        const reviewers = req.body.pullRequestReviewers.split(',');
-        reviewers.forEach(function (reviewer) {
-            console.log(reviewer);
-
-            let cause = 'created a new';
-            if (req.body.buttonTrigger)
-                cause = 'kindly asks you to review his'
-            let message = `*${req.body.pullRequestAuthorDisplayName}* ${cause} Pull Request: _${req.body.pullRequestTitle}_ \nPlease review at:`;
-            request.post({
-                url: webHookUrl,
-                json: {
-                    channel: '@' + reviewer,
-                    text: message,
-                    "attachments": [
-                        {
-                            "color": "#3AA3E3",
-                            "text": `${req.body.pullRequestUrl}`,
-                        },
-                        {
-                            "color": "#F35A00",
-                            "text": "Will you do it right now?",
-                            "actions": [
-                                {
-                                    "name": "yes",
-                                    "text": "Yes master!",
-                                    "style": "danger",
-                                    "type": "button",
-                                    "value": "yes"
-                                }
-                            ],
-                            //"footer": "Yes I'm a bot (c) :robot_face:"                         
-                        }
-                    ]
+function sendMessage(channel, message, pullRequestUrl, color) {
+    request.post({
+        proxy: variables.proxy,
+        url: slackUrl,
+        form: {
+            token: variables.botToken,
+            channel: '@' + channel,
+            text: message,
+            as_user: true,
+            attachments: JSON.stringify([
+                {
+                    color: color,
+                    text: pullRequestUrl,
+                    footer: "Yes I'm a bot (c) :robot_face:"
                 }
-            }, function (error, response, body) {
-                console.log(body);
+            ])
+        }
+    }, function (error, response, body) {
+        console.log(body);
+    });
+}
+
+app.post('/' + variables.urlPath, function (req, res) {
+    console.log(req.body);
+    if (req.body) {
+        if (req.body.pullRequestAction == 'OPENED' || req.body.pullRequestAction == 'BUTTON_TRIGGER') {
+            const reviewers = req.body.pullRequestReviewers.split(',');
+            reviewers.forEach(function (reviewer) {
+                let cause = 'created a new';
+                if (req.body.buttonTrigger)
+                    cause = 'kindly asks you to review his';
+                let message = `*${req.body.pullRequestAuthorDisplayName}* ${cause} Pull Request: _${req.body.pullRequestTitle}_ \nPlease review at:`;
+                sendMessage(reviewer, message, req.body.pullRequestUrl, "#3AA3E3");
             });
-        });
+        } else if (req.body.pullRequestAction == 'APPROVED') {
+            if (req.body.pullRequestApprovedCount >= variables.requiredApprovals) {
+                let successMessage = `*Hurray*!!! Your Pull Request _${req.body.pullRequestTitle}_ reached required approval count and ready to be merged! \nCome on click *Merge* at:`;
+                sendMessage(req.body.pullRequestAuthor, successMessage, req.body.pullRequestUrl, "#3DB014");
+            }
+        }
     }
     res.send(req.body);
 });
 
-app.listen(9999);
-console.log('Notifier server started at port ' + port);
+app.listen(variables.port);
+console.log('Notifier server started at port ' + variables.port);
