f87cb60e70726dcaec3235e4a48b873f21ac2421
iminin <Igor.Minin@t-systems.ru>
2017-02-14 14:38:27 +0300

    Jenkins fix

--- notifier.js
+++ notifier.js
@@ -37,33 +37,33 @@ app.post('/' + variables.urlPath, function (req, res) {
     if (req.body) {
         if (req.body.conflict) {
             // Notify on PR conflicts
-            const conflictMessage = `*Warning*! Your Pull Request _${req.body.pullRequestTitle}_ has conflicts with branch *${req.body.toBranch}* \nSee conflicts at:`;
+            const conflictMessage = `:crossed_swords:  *Warning*! Your Pull Request _${req.body.pullRequestTitle}_ has conflicts with branch *${req.body.toBranch}* \nSee conflicts at:`;
             sendMessage(req.body.pullRequestAuthor, conflictMessage, req.body.pullRequestUrl, '#D21111');
         } else if (req.body.pullRequestAction == 'OPENED' || req.body.pullRequestAction == 'BUTTON_TRIGGER') {
             // Notify on opened PR or button click
             const reviewers = req.body.pullRequestReviewers.split(',');
             reviewers.forEach(function (reviewer) {
-                let cause = 'created a new';
+                let cause = `:inbox_tray: *${req.body.pullRequestAuthorDisplayName}* created a new`;
                 // Change text if button clicked
                 if (req.body.pullRequestAction == 'BUTTON_TRIGGER')
-                    cause = '*reminds* you to review his';
-                const message = `*${req.body.pullRequestAuthorDisplayName}* ${cause} Pull Request: _${req.body.pullRequestTitle}_ \nPlease review at:`;
+                    cause = `:bell: *${req.body.pullRequestAuthorDisplayName}* *reminds* you to review his`;
+                const message = `${cause} Pull Request: _${req.body.pullRequestTitle}_ \nPlease review at:`;
                 sendMessage(reviewer, message, req.body.pullRequestUrl, '#3AA3E3');
             });
         } else if (req.body.pullRequestAction == 'APPROVED') {
             // Notify if required approvals are reached
             if (req.body.pullRequestApprovedCount >= variables.requiredApprovals) {
-                const successMessage = `*Hurray*!!! Your Pull Request _${req.body.pullRequestTitle}_ reached required approval count and ready to be merged! \nCome on click *Merge* at:`;
+                const successMessage = `:tada: *Hurray*!!! Your Pull Request _${req.body.pullRequestTitle}_ reached required approval count and ready to be merged! \nCome on click *Merge* at:`;
                 sendMessage(req.body.pullRequestAuthor, successMessage, req.body.pullRequestUrl, '#3DB014');
             }
         } else if (req.body.pullRequestAction == 'COMMENTED') {
             // Notify on new comment if it's not own comment
-            if (req.body.triggerUser == variables.ciUserName && req.body.comment.indexOf('BUILD FAILED') >= 0) {
+            if (req.body.triggerUser == variables.ciUserName && req.body.buildFailed) {
                 // Notify if ci commented that build failed
-                const buildFailedMessage = `*Warning*! Your Pull Request _${req.body.pullRequestTitle}_ just failed to build on ${variables.ciUserName} \nSee at:`;
+                const buildFailedMessage = `:x: *Warning*! Your Pull Request _${req.body.pullRequestTitle}_ just failed to build on ${variables.ciUserName} \nSee at:`;
                 sendMessage(req.body.pullRequestAuthor, buildFailedMessage, req.body.pullRequestUrl, '#D21111');
             } else if (req.body.triggerUser != req.body.pullRequestAuthor && req.body.triggerUser != variables.ciUserName) {
-                const commentMessage = `*${req.body.triggerDisplayName}* commented on your Pull Request _${req.body.pullRequestTitle}_:\n ${req.body.comment} \nReply at:`;
+                const commentMessage = `:speech_balloon: *${req.body.triggerDisplayName}* commented on your Pull Request _${req.body.pullRequestTitle}_:\n ${req.body.comment} \nReply at:`;
                 sendMessage(req.body.pullRequestAuthor, commentMessage, req.body.pullRequestUrl, '#EFC058');
             }
         }
