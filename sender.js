f297b3bc95541f9972b4f2d8d8f1058d8451f33f
iminin <Igor.Minin@t-systems.ru>
2017-10-16 15:25:40 +0300

    Added proper responses for errors

--- sender.js
+++ sender.js
@@ -15,7 +15,7 @@ function getUserChannel(user) {
     return channel;
 }
 
-function sendMessage(user, message, pullRequestUrl, color) {
+function sendMessage(user, message, pullRequestUrl, color, expressResponse) {
     const channel = getUserChannel(user);
     console.log(channel);
     if (channel) {
@@ -37,9 +37,12 @@ function sendMessage(user, message, pullRequestUrl, color) {
             }
         }, function (error, response, body) {
             console.log(body);
+            expressResponse.status(500).send(body);
         });
     } else {
-        console.log(`User "${user}" is not mapped to a slack channel in users.json`);
+        const message = `User "${user}" is not mapped to a slack channel in users.json`;
+        console.log(message);
+        expressResponse.status(500).send(message);
     }
 }
 
@@ -51,7 +54,7 @@ module.exports = function (app) {
             if (req.body.conflict) {
                 // Notify on PR conflicts
                 const conflictMessage = `:crossed_swords:  *Warning*! Your Pull Request _${req.body.pullRequestTitle}_ has conflicts with branch *${req.body.toBranch}* \nSee conflicts at:`;
-                sendMessage(req.body.pullRequestAuthor, conflictMessage, req.body.pullRequestUrl, '#D21111');
+                sendMessage(req.body.pullRequestAuthor, conflictMessage, req.body.pullRequestUrl, '#D21111', res);
             } else if (req.body.pullRequestAction == 'OPENED' || req.body.pullRequestAction == 'BUTTON_TRIGGER') {
                 // Notify on opened PR or button click
                 const reviewers = req.body.pullRequestReviewers.split(',');
@@ -61,29 +64,30 @@ module.exports = function (app) {
                     if (req.body.pullRequestAction == 'BUTTON_TRIGGER')
                         cause = `:bell: *${req.body.pullRequestAuthorDisplayName}* *reminds* you to review his`;
                     const message = `${cause} Pull Request: _${req.body.pullRequestTitle}_ \nPlease review at:`;
-                    sendMessage(reviewer, message, req.body.pullRequestUrl, '#3AA3E3');
+                    sendMessage(reviewer, message, req.body.pullRequestUrl, '#3AA3E3', res);
                 });
             } else if (req.body.pullRequestAction == 'APPROVED') {
                 // Notify if required approvals are reached
                 if (req.body.pullRequestApprovedCount >= variables.requiredApprovals) {
                     const successMessage = `:tada: *Hurray*!!! Your Pull Request _${req.body.pullRequestTitle}_ reached required approval count and ready to be merged! \nCome on click *Merge* at:`;
-                    sendMessage(req.body.pullRequestAuthor, successMessage, req.body.pullRequestUrl, '#3DB014');
+                    sendMessage(req.body.pullRequestAuthor, successMessage, req.body.pullRequestUrl, '#3DB014', res);
                 }
             } else if (req.body.pullRequestAction == 'COMMENTED') {
                 // Notify on new comment if it's not own comment
                 if (req.body.triggerUser == variables.ciUserName && req.body.buildFailed) {
                     // Notify if ci commented that build failed
                     const buildFailedMessage = `:x: *Warning*! Your Pull Request _${req.body.pullRequestTitle}_ just failed to build on ${variables.ciUserName} \nSee at:`;
-                    sendMessage(req.body.pullRequestAuthor, buildFailedMessage, req.body.pullRequestUrl, '#D21111');
+                    sendMessage(req.body.pullRequestAuthor, buildFailedMessage, req.body.pullRequestUrl, '#D21111', res);
                 } else if (req.body.triggerUser == variables.ciUserName && req.body.buildSuccessful) {
                     const buildSuccessfulMessage = `:white_check_mark: *Good Job*! Your Pull Request _${req.body.pullRequestTitle}_ was successfully build on ${variables.ciUserName} \nSee at:`;
-                    sendMessage(req.body.pullRequestAuthor, buildSuccessfulMessage, req.body.pullRequestUrl, '#00B300');
+                    sendMessage(req.body.pullRequestAuthor, buildSuccessfulMessage, req.body.pullRequestUrl, '#00B300', res);
                 } else if (req.body.triggerUser != req.body.pullRequestAuthor && req.body.triggerUser != variables.ciUserName) {
                     const commentMessage = `:speech_balloon: *${req.body.triggerDisplayName}* commented on your Pull Request _${req.body.pullRequestTitle}_:\n ${req.body.comment} \nReply at:`;
-                    sendMessage(req.body.pullRequestAuthor, commentMessage, req.body.pullRequestUrl, '#EFC058');
+                    sendMessage(req.body.pullRequestAuthor, commentMessage, req.body.pullRequestUrl, '#EFC058', res);
                 }
             }
+        } else {
+            res.send(req.body);
         }
-        res.send(req.body);
     });
 }
